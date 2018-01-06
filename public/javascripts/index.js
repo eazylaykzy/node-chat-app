47463ed1711b2703c7b56e074d790400c1986fd6
Eazylaykzy <eazylaykzy@gmail.com>
2018-01-06 18:55:12 +0100

    Emit newMessage on createMessage

---
+++ public/javascripts/index.js
@@ -0,0 +1,13 @@
+let socket = io();
+
+socket.on('connect', () => {
+  console.log('Connected to the server');
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected from the server');
+});
+
+socket.on('newMessage', message => {
+  console.log('New message', message);
+});
